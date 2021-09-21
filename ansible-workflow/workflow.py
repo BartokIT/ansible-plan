369359808e3c8307c38c4e1b537c939029673a49
Claudio Papa <claudio.papa@bancaditalia.it>
2021-09-21 10:04:23 +0000

    Modified printing layout and added command line options

--- ansible-workflow/workflow.py
+++ ansible-workflow/workflow.py
@@ -1,17 +1,15 @@
-#!/bin/env python3
-import ansible_runner
 import networkx as nx
-from numpy import e
 import yaml
 import matplotlib.pyplot as plt
+import ansible_runner
+import abc
+import inspect
 import random
 import string
-import inspect
-import abc
-import time
 import os
-import copy
-import curses
+import time
+from datetime import datetime
+
 
 class Node():
     __metaclass__ = abc.ABCMeta
@@ -37,11 +35,12 @@ class BNode(Node):
         return 'ended'
 
 class PNode(Node):
-    def __init__(self, id, playbook, inventory, extravars={}):
+    def __init__(self, id, playbook, inventory, artifact_dir, extravars={}):
         super(PNode, self).__init__(id)
         self.__playbook = playbook
         self.__inventory = inventory
         self.__extravars = extravars
+        self.__artifact_dir = artifact_dir
         self.__thread = None
         self.__runner = None
 
@@ -64,115 +63,27 @@ class PNode(Node):
         return self.__playbook
 
     def run(self):
-        self.__thread, self.__runner = ansible_runner.run_async(playbook=self.__playbook, inventory=self.__inventory, extravars=self.__extravars,  quiet=True)
+        self.__thread, self.__runner = ansible_runner.run_async(playbook=self.__playbook,
+                                                                inventory=self.__inventory,
+                                                                ident=self.get_id(),
+                                                                artifact_dir=self.__artifact_dir,
+                                                                extravars=self.__extravars,  quiet=True)
+
 
 def nudge(pos, x_shift, y_shift):
     return {n:(x + x_shift, y + y_shift) for n,(x,y) in pos.items()}
 
-class CursesGui():
-    def __init__(self):
-        self.__main_window = None
-        self.__progress_window = None
-        self.__progress_window_width = 20
-        self.__window_margin = 3
-        self.__first_draw = True
-        self.__row = 0
-        terminal = curses.initscr()
-        self.__define_color()
-
-        self.__max_y, self.__max_x = terminal.getmaxyx()
-        self.__main_window = curses.newwin(self.__max_y - self.__window_margin , self.__max_x - self.__window_margin - self.__progress_window_width, self.__window_margin, self.__window_margin)
-        self.__progress_window = curses.newwin(self.__max_y - self.__window_margin, self.__progress_window_width,  self.__window_margin, self.__max_x - self.__window_margin - self.__progress_window_width)
-        self.__status_map = {'not_started': {'label': 'not started', 'color': 1},
-                             'running': {'label': 'running', 'color': 2},
-                             'failed': {'label': 'failed', 'color': 3},
-                             'ended': {'label': 'ended', 'color': 4} }
-
-    def __define_color(self):
-        curses.start_color()
-        curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_WHITE, -1) # not started
-        curses.init_pair(2, curses.COLOR_YELLOW, -1) # running
-        curses.init_pair(3, curses.COLOR_RED, -1) # failed
-        curses.init_pair(4, curses.COLOR_GREEN, -1) # ended
-
-    def print_status(self, input_tree, graph):
-        import_tree = copy.deepcopy(input_tree)
-        del import_tree[0]
-        del import_tree[len(import_tree) - 1]
-
-        #self.__main_window.addstr(0, 0, '┬' )
-        self.__row = 0
-        #self.__main_window.clear()
-        self.__progress_window.clear()
-        self.__print_tree(import_tree, graph)
-        # refresh
-        if self.__first_draw:
-            self.__main_window.refresh()
-        self.__progress_window.refresh()
-        curses.napms(1000)
-        self.__first_draw = False
-
-    def __print_tree(self,  nodes, graph, prev_latest=False, level=0, prefix=''):
-        for inode in nodes:
-            # selection of char for current node
-            current_char = '├'
-            previous_char = '│'
-            next_prefix = ''
-
-            if nodes[-1] == inode: # latest element
-                current_char = '└'
-
-                if nodes[0] == inode: # is composed by one element
-                    current_char = '─'
-                    if not prev_latest:
-                        previous_char = '├'
-                else:
-                    if prev_latest:
-                        previous_char = ' '
-            elif nodes[0] == inode: # is the first
-                current_char = '┬'
-                if prev_latest:
-                    previous_char = '└'
-                else:
-                    previous_char = '├'
-            elif prev_latest:
-                previous_char = ' '
-
-            # prefix
-            if level > 0:
-                next_prefix = '│'
-                if prev_latest:
-                    next_prefix = ' '
-
-
-            #print("node: %s | level: %s | prev_latest: %s | prev_char: %s | current_char: %s | next_prefix: #%s# | prefix: #%s#" % ( inode['id'], level, prev_latest, previous_char, current_char, next_prefix, prefix))
-            if 'block' in inode:
-                self.__print_tree(inode['block'], graph, prev_latest=(nodes[-1] == inode), level=(level + 1), prefix=prefix+next_prefix)
-            else:
-                if level == 0:
-                    previous_char = ''
-                # draw tree node
-                tree_string =  "%s%s%s %s - %s" % (prefix, previous_char, current_char, inode['id'], inode['import_playbook'])
-                self.__main_window.addstr(self.__row, 0, tree_string)
-                # draw filling dots
-                self.__main_window.addstr(self.__row, len(tree_string.strip()) + 1, "."*(self.__max_x - len(tree_string.strip()) - 5 - self.__window_margin - self.__progress_window_width))
-                # draw status
-                node_status =   graph.nodes[inode['id']]['data'].get_status()
-                status_label = self.__status_map[node_status]['label']
-                status_color  = self.__status_map[node_status]['color']
-                self.__progress_window.addstr(self.__row, 1, "%s" % status_label, curses.color_pair(status_color))
-                self.__row = self.__row + 1
-
 
 class AnsibleWorkflow():
-    def __init__(self, workflow, inventory):
+    def __init__(self, workflow, inventory, logging_dir):
         self.__graph = nx.DiGraph()
         self.__allowed_node_keys = set(['block', 'import_playbook', 'name', 'strategy', 'id', 'vars'])
         self.__workflow_filename = workflow
         self.__inventory_filename = inventory
         self.__running_nodes = ['s']
         self.__running_statues = 'not_started'
+        self.__logging_dir = logging_dir
+        self.__data = dict()
 
         # import data from file
         self.__import_file(self.__workflow_filename)
@@ -198,7 +109,7 @@ class AnsibleWorkflow():
         nx.draw_networkx_nodes(self.__graph, pos, node_size=250)
 
         # draw block nodes differently
-        block_nodes=[n for n, d in  list(self.__graph.nodes(data=True)) if isinstance(d['data'], BNode) and d['data'].get_id() != 's' and d['data'].get_id() != 'e']
+        block_nodes=[n for n in  list(self.__graph.nodes()) if isinstance(self.__data[n]['object'], BNode) and self.__data[n]['object'].get_id() != 's' and self.__data[n]['object'].get_id() != 'e']
         nx.draw_networkx_nodes(self.__graph, pos, nodelist=block_nodes, node_size=350, node_color="#777")
 
         # draw start and end node differently
@@ -209,27 +120,22 @@ class AnsibleWorkflow():
 
         # draw labels
         label_position = nudge(pos, 0, 20)
-        labels = {n: '' if isinstance(d['data'], BNode) else os.path.basename(d['data'].get_playbook()) for n, d in  list(self.__graph.nodes(data=True)) }
+        labels = {n: '' if isinstance(self.__data[n]['object'], BNode) else os.path.basename(self.__data[n]['object'].get_playbook()) for n in list(self.__graph.nodes()) }
         nx.draw_networkx_labels(self.__graph,labels=labels, pos=label_position, font_size=10)
 
         plt.savefig(self.__workflow_filename + '.png')
 
+    def is_running(self):
+        return len(self.__running_nodes) != 0
 
-    def run_graphically(self):
-        # review to use separated thread
-        if self.__running_statues != 'not_started':
-            raise Exception("Already running")
-        self.__running_statues = 'started'
-        cg = CursesGui()
-        # loop over running nodes
-        while len(self.__running_nodes):
-            self.__run_step()
-            cg.print_status(self.__input_file_data, self.__graph)
+    def get_graph(self):
+        return self.__graph
 
-        curses.endwin()
+    def get_node_datas(self):
+        return self.__data
 
-        self.__running_statues = 'ended'
-
+    def get_input_data(self):
+        return self.__input_file_data
 
     def _check_node_syntax(self, node):
         remaining_keys = set(node.keys()) - self.__allowed_node_keys
@@ -269,11 +175,12 @@ class AnsibleWorkflow():
                 playbook=os.path.abspath(inode['import_playbook'])
                 inventory=os.path.abspath(inode.get('inventory', self.__inventory_filename))
                 extravars=inode.get('vars', {})
-                gnode = PNode(gnode_id, playbook=playbook, inventory=inventory, extravars=extravars)
+                gnode = PNode(gnode_id, playbook=playbook, inventory=inventory, artifact_dir=self.__logging_dir, extravars=extravars)
                 print("     %s node: %s       playbook: %s     inventory: %s    vars: %s" % (indentation, gnode_id, playbook, inventory, extravars))
 
             # the node specification is added
-            self.__graph.add_node(gnode_id, data=gnode)
+            self.__graph.add_node(gnode_id)
+            self.__data[gnode_id]=dict(object=gnode)
 
 
             if 'block' not in inode:
@@ -294,34 +201,35 @@ class AnsibleWorkflow():
         return zero_outdegree_nodes
 
     def __is_node_runnable(self, node_id):
-        print("Check node %s can be run" % node_id)
+        #print("Check node %s can be run" % node_id)
         in_edges = self.__graph.in_edges(node_id)
         for edge in in_edges:
             previous_node = edge[0]
-            print("\tPrevious node %s status: %s" % (previous_node, self.__graph.nodes[previous_node]['data'].get_status()))
-            if self.__graph.nodes[previous_node]['data'].get_status() != 'ended':
+            #print("\tPrevious node %s status: %s" % (previous_node, self.__graph.nodes[previous_node]['data'].get_status()))
+            if self.__data[previous_node]['object'].get_status() != 'ended':
                 return False
         return True
 
     def __run_step(self):
         for node_id in self.__running_nodes:
-
-            node = self.__graph.nodes[node_id]['data']
+            node = self.__data[node_id]['object']
             # if current node is ended search for next nodes
             if node.get_status() == 'ended':
                 self.__running_nodes.remove(node_id)
+                self.__data[node_id]['ended'] = datetime.now()
                 for out_edge in self.__graph.out_edges(node_id):
                     next_node_id = out_edge[1]
-                    next_node = self.__graph.nodes[next_node_id]['data']
-
+                    next_node = self.__data[next_node_id]['object']
                     if self.__is_node_runnable(next_node_id):
-                        print("Run node %s" % next_node_id)
+                        #print("Run node %s" % next_node_id)
                         self.__running_nodes.append(next_node_id)
                         if isinstance(next_node , PNode):
+                            self.__data[next_node_id]['started'] = datetime.now()
                             next_node.run()
             elif node.get_status() == 'failed':
                 # just remove a failed node
-                print("Failed node %s" % node_id)
+                #print("Failed node %s" % node_id)
+                self.__data[node_id]['ended'] = datetime.now()
                 self.__running_nodes.remove(node_id)
 
     def run(self):
@@ -331,16 +239,5 @@ class AnsibleWorkflow():
         # loop over nodes
         while len(self.__running_nodes):
             self.__run_step()
+            time.sleep(1)
         self.__running_statues = 'ended'
-
-def main(stdscr):
-    aw = AnsibleWorkflow(workflow="input2.yml", inventory='inventory.ini')
-    #aw.draw_graph()
-    #aw.print_graph()
-    aw.run_graphically()
-    #aw.run()
-    #stdscr.getch()
-
-
-if __name__ == "__main__":
-    curses.wrapper(main)
