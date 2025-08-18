8a83518bd97a191126d45b02bab74adee23bc953
Claudio Papa <claudio.papa@bancaditalia.it>
2025-08-18 16:41:28 +0200

    Improved style

--- frontend/ansible_workflow_frontend/output.py
+++ frontend/ansible_workflow_frontend/output.py
@@ -425,8 +425,7 @@ class TextualWorkflowOutput(WorkflowOutput):
                 details_table.add_column("Value")
 
             def add_detail(key, value):
-                padded_key = f" {key} "
-                details_table.add_row(Text(padded_key.ljust(20), style="bold white on dark_blue"), value, height=None)
+                details_table.add_row(key, value, height=None)
 
             add_detail("ID", node_data.get('id'))
             if node_data.get('type') == 'playbook':
